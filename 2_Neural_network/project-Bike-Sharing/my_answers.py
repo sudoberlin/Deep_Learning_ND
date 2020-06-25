702c3a8d30ef8c5c4846416943e47bab702e10d7
sudoberlin <shubham4163@nitnagaland.ac.in>
2020-06-25 23:17:33 +0530

    Project_Dog_Breed_Classifier

--- 2_Neural_network/project-Bike-Sharing/my_answers.py
+++ 2_Neural_network/project-Bike-Sharing/my_answers.py
@@ -141,7 +141,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 5000
-learning_rate = 0.2
-hidden_nodes = 64
+iterations = 4000
+learning_rate = 0.5
+hidden_nodes = 16
 output_nodes = 1
